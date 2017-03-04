b1036eaf9e388ea6c51a1bb669864011ce4da589
Matt <matthew.w.lummus@gmail.com>
2017-03-03 21:56:16 -0600

    Update hockeyRemote.js

--- hockeyRemote.js
+++ hockeyRemote.js
@@ -1,3 +1,43 @@
-$(function () {
-  console.log($, 'hockeyRemote.js test');
+// Load jQuery
+(function(e, s) {
+    e.src = s;
+    e.onload = function() {
+        jQuery.noConflict();
+        console.log('jQuery injected');
+    };
+    document.head.appendChild(e);
+})(document.createElement('script'), 'code.jquery.com/jquery-latest.min.js')
+
+(function () {
+  function init() {
+    if (!$) {
+      fetch$();
+      return;
+    } // end if (!$)
+    
+    console.log('jQuery present.');
+    main();
+    
+    function fetch$() {
+      var interval;
+      
+      if (jQuery) {
+        $ = jQuery;
+        return;
+      } // end if(jQuery)
+    
+      setInterval(intervalFN, 250)
+      
+      function intervalFN() {
+        if ($ || jQuery) {
+          $      = $ || jQuery; // sets to self if self is present
+          jQuery = $ || jQuery; // sets to other if other is present and self is null
+        
+          clearInterval(interval);
+          main();
+          return;
+        } // end if ($ || jQuery)
+      } // end function intervalFN
+    }  // end function fetch$
+  } // end function init
 })();
